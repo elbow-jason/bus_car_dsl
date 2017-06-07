a83a16b9c5c1c954b7b2284dd6a5636dbb7f4157
Jason Goldberger <jlgoldb2@asu.edu>
2017-06-06 18:52:54 -0700

    bumped version to 0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BusCarDsl.Mixfile do
   def project do
     [
       app: :bus_car_dsl,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
